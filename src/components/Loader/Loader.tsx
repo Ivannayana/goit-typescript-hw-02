5574b6ad56971d309432a840108e26b86b6c2301
Ivannayana <151822259+Ivannayana@users.noreply.github.com>
2024-11-23 14:14:58 +0200

    task performance

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,15 @@
+import { DNA } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <DNA
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="dna-loading"
+      wrapperStyle={{}}
+    />
+  );
+};
+
+export default Loader;
